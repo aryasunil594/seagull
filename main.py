acf2e3d4fb4e606d2a9a84df86a6c5c2f549c4da
aryasunil594 <aryasunilarya04@gmail.com>
2025-01-16 14:46:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 st.title("Flower Classification App")
 
 file_name = "lr_model.pkl"
-with open(path.join(file_name), 'rb')as f:
+with open(path.join(file_name) , 'rb') as f:
     lr_model = pickle.load(f)
 
 sl = st.number_input("Insert a sepel length")
@@ -15,6 +15,6 @@ sw = st.number_input("Insert a sepel width")
 pl = st.number_input("Insert a petal length")
 pw = st.number_input("Insert a petal width")
 
-if st.button("predict"):
-    pred = lr_model.predict(np.array([[sl,sw,pl,pw]]))
-    st.write("The flower is:", pred[0])
+if st.button("Predict"):
+    pred = lr_model.predict(np.array([[sl, sw, pl, pw]]))
+    st.write("The flower is :", pred[0])
